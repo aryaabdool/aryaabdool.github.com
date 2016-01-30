52c5cee7e8b2e3b847b46dcc14e863bf1a9a0ca4
Arya Abdool <ahhreeuhh@gmail.com>
2016-01-30 03:01:26 -0500

    icons

--- scripts/resume.js
+++ scripts/resume.js
@@ -4,18 +4,36 @@ $(document).ready(function(){
    		$(".experience-main").hide();
    		$(".interests-main").hide();
    		$('.education-main').show();
+   		$("img.placeholder-proto.edu-proto").show();
+   		$("img.placeholder-proto.experience-proto").hide();
+   		$("img.placeholder-proto.interest-proto").hide();
    });
 
    $(".experience-button").on("click", function(){
    		$(".education-main").hide();
    		$(".interests-main").hide();
    		$(".experience-main").show();
+   		$("img.placeholder-proto.experience-proto").show();
+   		$("img.placeholder-proto.edu-proto").hide();
+   		$("img.placeholder-proto.interest-proto").hide();
    });
 
    $(".interests-button").on("click", function(){
    		$(".education-main").hide();
    		$(".experience-main").hide();
    		$(".interests-main").show();
+   		$("img.placeholder-proto.interest-proto").show();
+   		$("img.placeholder-proto.edu-proto").hide();
+   		$("img.placeholder-proto.experience-proto").hide();
+   });
+
+   $('a.resume').on('click', function(){
+   		$(".education-main").hide();
+   		$(".interests-main").hide();
+   		$(".experience-main").show();
+   		$("img.placeholder-proto.experience-proto").show();
+   		$("img.placeholder-proto.edu-proto").hide();
+   		$("img.placeholder-proto.interest-proto").hide();
    });
 
 });
